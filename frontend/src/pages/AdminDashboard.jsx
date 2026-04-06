0a1cad2da850aa10c404e5b9ef81dc5180727790
K.VARNITHAN <b23cs117@kitsw.ac.in>
2026-04-06 23:52:45 +0530

    Fixed missing icons in AdminDashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import API_BASE_URL from '../config';
 import { useNavigate } from 'react-router-dom';
+import { Power, PlusCircle } from 'lucide-react';
 
 export default function AdminDashboard() {
   const [candidates, setCandidates] = useState([]);
